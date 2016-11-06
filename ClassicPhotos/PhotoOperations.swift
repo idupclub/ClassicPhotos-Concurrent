2047098391bc0511514f5c997046fb2602842c3c
Chun Cao <chun@Chuns-iMac.local>
2016-11-06 18:23:15 +0800

    init

---
+++ ClassicPhotos/PhotoOperations.swift
@@ -0,0 +1,150 @@
+//
+//  PhotoOperations.swift
+//  ClassicPhotos
+//
+//  Created by Richard Turton on 17/04/2015.
+//  Copyright (c) 2015 raywenderlich. All rights reserved.
+//
+
+import Foundation
+import UIKit
+// FIXME: comparison operators with optionals were removed from the Swift Standard Libary.
+// Consider refactoring the code to use the non-optional operators.
+fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+  switch (lhs, rhs) {
+  case let (l?, r?):
+    return l < r
+  case (nil, _?):
+    return true
+  default:
+    return false
+  }
+}
+
+// FIXME: comparison operators with optionals were removed from the Swift Standard Libary.
+// Consider refactoring the code to use the non-optional operators.
+fileprivate func > <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+  switch (lhs, rhs) {
+  case let (l?, r?):
+    return l > r
+  default:
+    return rhs < lhs
+  }
+}
+
+
+// This enum contains all the possible states a photo record can be in
+enum PhotoRecordState {
+    case new, downloaded, filtered, failed
+}
+
+class PhotoRecord {
+    let name:String
+    let url:URL
+    var state = PhotoRecordState.new
+    var image = UIImage(named: "Placeholder")
+    
+    init(name:String, url:URL) {
+        self.name = name
+        self.url = url
+    }
+}
+
+class PendingOperations {
+    lazy var downloadsInProgress = [IndexPath:Operation]()
+    lazy var downloadQueue:OperationQueue = {
+        var queue = OperationQueue()
+        queue.name = "Download queue"
+//        queue.maxConcurrentOperationCount = 5
+        return queue
+    }()
+    
+    lazy var filtrationsInProgress = [IndexPath:Operation]()
+    lazy var filtrationQueue:OperationQueue = {
+        var queue = OperationQueue()
+        queue.name = "Image Filtration queue"
+//        queue.maxConcurrentOperationCount = 5
+        return queue
+    }()
+}
+
+class ImageDownloader: Operation {
+    //1
+    let photoRecord: PhotoRecord
+    
+    //2
+    init(photoRecord: PhotoRecord) {
+        self.photoRecord = photoRecord
+    }
+    
+    //3
+    override func main() {
+        //4
+        if self.isCancelled {
+            return
+        }
+        //5
+        let imageData = try? Data(contentsOf: self.photoRecord.url)
+        
+        //6
+        if self.isCancelled {
+            return
+        }
+        
+        //7
+        if imageData?.count > 0 {
+            self.photoRecord.image = UIImage(data:imageData!)
+            self.photoRecord.state = .downloaded
+        }
+        else
+        {
+            self.photoRecord.state = .failed
+            self.photoRecord.image = UIImage(named: "Failed")
+        }
+    }
+}
+
+class ImageFiltration: Operation {
+    let photoRecord: PhotoRecord
+    
+    init(photoRecord: PhotoRecord) {
+        self.photoRecord = photoRecord
+    }
+    
+    override func main () {
+        if self.isCancelled {
+            return
+        }
+        
+        if self.photoRecord.state != .downloaded {
+            return
+        }
+        
+        if let filteredImage = self.applySepiaFilter(self.photoRecord.image!) {
+            self.photoRecord.image = filteredImage
+            self.photoRecord.state = .filtered
+        }
+    }
+    
+    func applySepiaFilter(_ image:UIImage) -> UIImage? {
+        let inputImage = CIImage(data:UIImagePNGRepresentation(image)!)
+        
+        if self.isCancelled {
+            return nil
+        }
+        let context = CIContext(options:nil)
+        let filter = CIFilter(name:"CISepiaTone")
+        filter!.setValue(inputImage, forKey: kCIInputImageKey)
+        filter!.setValue(0.8, forKey: "inputIntensity")
+        let outputImage = filter!.outputImage
+        
+        if self.isCancelled {
+            return nil
+        }
+        
+        let outImage = context.createCGImage(outputImage!, from: outputImage!.extent)
+        let returnImage = UIImage(cgImage: outImage!)
+        return returnImage
+    }
+}
+
